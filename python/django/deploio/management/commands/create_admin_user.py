6ee244ee18fd31f00daeb598549e6c4fba340274
Sebastian Nickel <nick@nine.ch>
2023-09-06 11:58:10 +0200

    remove unused management command

--- python/django/deploio/management/commands/create_admin_user.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-
-class Command(BaseCommand):
-    help = 'Creates an admin user if none exists'
-
-    def handle(self, *args, **kwargs):
-        self.stdout.write("blablubb")
