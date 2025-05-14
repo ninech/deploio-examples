4548b839d762842ee4a6ac5a55df2d85bc4373c2
David Buchmann <david.buchmann@liip.ch>
2025-05-14 13:35:19 +0200

    setup symfony to connect to database, kvs and s3

--- php/symfony/config/bundles.php
+++ php/symfony/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Webf\Flysystem\DsnBundle\WebfFlysystemDsnBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
--- php/symfony/src/Controller/HomeController.php
+++ php/symfony/src/Controller/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,8 +16,47 @@ use Symfony\Component\HttpFoundation\Response;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $orm, CacheItemPoolInterface $app, FilesystemOperator $persistentStorage, LoggerInterface $logger): Response
     {
-        return $this->render('deploio.html.twig');
+
+        try {
+            $status['database'] = is_string($orm->getConnection()->getDatabase());
+        } catch (Exception $e) {
+            $logger->warning('Error while trying to use database: "{message}"', [
+                'message' => $e->getMessage(),
+                'exception' => $e,
+            ]);
+            $status['database'] = false;
+        }
+
+        try {
+            $item = $app->getItem('foo');
+            $item->set('check');
+            $app->save($item);
+            // if the cache is not working, there is no exception to not interrupt the program flow, but nothing is stored
+            $status['kvs'] = $app->hasItem('foo');
+        } catch (InvalidArgumentException $e) {
+            $logger->warning('Error while trying to use key value store: "{message}"', [
+                'message' => $e->getMessage(),
+                'exception' => $e,
+            ]);
+            $status['kvs'] = false;
+        }
+
+        try {
+            $persistentStorage->write('/test-file', 'Symfony Test');
+            $persistentStorage->has('/test-file');
+            $status['storage'] = true;
+        } catch (FilesystemException $e) {
+            $logger->warning('Error while trying to connect to filesystem: "{message}"', [
+                'message' => $e->getMessage(),
+                'exception' => $e,
+            ]);
+            $status['storage'] = false;
+        }
+
+        return $this->render('deploio.html.twig', [
+            'status' => $status,
+        ]);
     }
 }
