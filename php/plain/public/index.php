d50f5d50417bf8791116089acab6f45dd7f0669f
Dirk <dirk@nine.ch>
2025-10-23 15:33:03 +0200

    fix

--- php/plain/public/index.php
+++ php/plain/public/index.php
@@ -63,10 +63,10 @@ $dbpass = getenv('DBPASS');
 $dbhost = getenv('DBHOST');
 
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'")
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($link, $test_query)
+$result = mysqli_query($link, $test_query);
 
 $tblCnt = 0;
 while($tbl = mysqli_fetch_array($result)) {
